da6c880cfbb23b2bd949f530fcc3f65c28936023
Mahabsd <Mahabsd@gmail.com>
2021-04-13 20:24:40 +0100

    covert excel to json

--- routes/fileUploadApi.js
+++ routes/fileUploadApi.js
@@ -2,7 +2,10 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const router = express.Router();
-
+const model = ['LastName', 'FirstName', 'Language', 'PayId', 'PayId2', 'PayId3', 'PayId4', 'PayId5', 'PayId6', 'Mail', 'ManagerMail', 'ManagerPayId', 'IsAdmin', 'IsAccountant', 'Tags', 'LocalCountry', 'LocalCurrency', 'ReviewerMail', 'ReviewerPayId', 'DefaultProjectExternalId', 'IsActive', 'MailAlias', 'MileageRate', 'IKReference']
+let increment = 0
+'use strict';
+const excelToJson = require('convert-excel-to-json');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -25,8 +28,15 @@ const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 // Routes
 router.post('/upload-single', upload.single('file'), async (req, res) => {
-
-    res.json({ message: 'file has been uploaded successfully!'});
+    console.log(req);
+    const result = excelToJson({
+        sourceFile: req.file.path
+    });
+    console.log(result);
+    for (const [key, value] of Object.entries(result.Sheet1[0])) {
+        model.includes(value) ? console.log(`${key} ${value} nombre : ${increment += 1}`) : console.log(value);
+    }
+    res.json({ message: 'file has been uploaded successfully!', content : result.Sheet1[0] });
 });
 
 module.exports = router;
